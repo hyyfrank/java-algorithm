59ed91479e35d813703060e7b8c8ba48644837b6
frank <hyyfrank@gmail.com>
2019-07-03 23:44:16 +0800

    🔥🔥🔥[Leecode-27] completed

--- src/main/java/com/autodesk/leecode/stringpart/StringEntry.java
+++ src/main/java/com/autodesk/leecode/stringpart/StringEntry.java
@@ -1,9 +1,12 @@
 package com.autodesk.leecode.stringpart;
 
+import com.autodesk.leecode.stringpart.L_27.L27_ALGO;
+
 public class StringEntry {
     public static void runStringAlgo() {
         System.out.println("start string test.");
         System.out.println("L27:移除元素");
+        L27_ALGO.run();
         System.out.println("L26:移除元素-有序表");
         System.out.println("L80:移除元素-有序表II");
         System.out.println("L277:搜寻名人");
---
+++ src/main/java/com/autodesk/leecode/util/helper.java
@@ -0,0 +1,14 @@
+package com.autodesk.leecode.util;
+
+import java.util.Arrays;
+
+public class helper {
+    public static void printInStuff(int[] target) {
+        System.out.println("输入:");
+        System.out.println(Arrays.toString(target));
+    }
+    public static void printOutStuff(int[] target) {
+        System.out.println("输出:");
+        System.out.println(Arrays.toString(target));
+    }
+}
