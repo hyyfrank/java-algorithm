29cd96cf39a3dc52172954ec363ab457b7226e39
frank <hyyfrank@gmail.com>
2019-07-03 22:52:44 +0800

    init

---
+++ src/main/java/com/autodesk/leecode/stringpart/StringEntry.java
@@ -0,0 +1,11 @@
+package com.autodesk.leecode.stringpart;
+
+public class StringEntry {
+    public static void runStringAlgo() {
+        System.out.println("start string test.");
+        System.out.println("L27:移除元素");
+        System.out.println("L26:移除元素-有序表");
+        System.out.println("L80:移除元素-有序表II");
+        System.out.println("L277:搜寻名人");
+    }
+}
---
+++ src/main/java/com/autodesk/leecode/tree/TreeEntry.java
@@ -0,0 +1,55 @@
+package com.autodesk.leecode.tree;
+
+public class TreeEntry {
+    public static void runTreeAlgo() {
+        System.out.println("start tree test.");
+        System.out.println("L144:移除元素");
+        System.out.println("L94:移除元素-有序表");
+        System.out.println("L145:移除元素-有序表II");
+        System.out.println("L102:搜寻名人");
+
+        System.out.println("*先序遍历相关*");
+        System.out.println("L100:搜寻名人");
+        System.out.println("L101:搜寻名人");
+        System.out.println("L226:搜寻名人");
+        System.out.println("L257:搜寻名人");
+        System.out.println("L112:搜寻名人");
+        System.out.println("L113:搜寻名人");
+        System.out.println("L129:搜寻名人");
+        System.out.println("L298:搜寻名人");
+        System.out.println("L111:搜寻名人");
+
+        System.out.println("*后序遍历相关*");
+        System.out.println("L104:搜寻名人");
+        System.out.println("L110:搜寻名人");
+        System.out.println("L124:搜寻名人");
+        System.out.println("L250:搜寻名人");
+        System.out.println("L366:搜寻名人");
+        System.out.println("L337:搜寻名人");
+
+
+        System.out.println("*广度优先搜索相关*");
+        System.out.println("L103:搜寻名人");
+        System.out.println("L107:搜寻名人");
+        System.out.println("L199:搜寻名人");
+
+        System.out.println("*二叉排序树相关*");
+        System.out.println("L98:搜寻名人");
+        System.out.println("L235:搜寻名人");
+        System.out.println("L236:搜寻名人");
+        System.out.println("L108:搜寻名人");
+        System.out.println("L109:搜寻名人");
+        System.out.println("L173:搜寻名人");
+        System.out.println("L230:搜寻名人");
+        System.out.println("L297:搜寻名人");
+        System.out.println("L285:搜寻名人");
+        System.out.println("L270:搜寻名人");
+        System.out.println("L272:搜寻名人");
+        System.out.println("L99:搜寻名人");
+
+
+
+
+
+    }
+}
