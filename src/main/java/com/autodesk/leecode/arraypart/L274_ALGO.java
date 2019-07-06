524543a984e2e21778e15ce0b313a640cbac6943
frank <hyyfrank@gmail.com>
2019-07-06 21:35:45 +0800

    🔥🔥🔥[ADD L274 update] Completed.

--- src/main/java/com/autodesk/leecode/arraypart/L274_ALGO.java
+++ src/main/java/com/autodesk/leecode/arraypart/L274_ALGO.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class L274_ALGO {
     public static void run() {
         int[] citations = {3,0,6,1,5};
-        int pos = HIndex(citations);
+        int pos = hIndexIII(citations);
         System.out.println("hindex:"+pos);
     }
 
-    public static int HIndex(int[] citations) {
+    public static int HIndexI(int[] citations) {
         Arrays.sort(citations);
         int result = 0;
         for(int i=citations.length-1; i>=0; i--){
@@ -22,5 +22,31 @@ public class L274_ALGO {
         }
 
         return result;
+    }
+    public static int HIndexII(int[] citations) {
+        int len = citations.length;
+        int[] counter = new int[len+1];
+        for(int c: citations){
+            counter[Math.min(len,c)]++;
+        }
+        int k=len;
+        for(int s=counter[len]; k > s; s += counter[k]){
+            k--;
+        }
+
+        return k;
+    }
+    public static int hIndexIII(int[] citations) {
+        int len = citations.length;
+        int low = 0;
+        int high = len - 1;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            if (len - mid > citations[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return len - low;
     }
 }
